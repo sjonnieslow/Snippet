b0fea233d43043e8a6b679146bbacff7dc1e3eb6
sjonnieslow <jd.weidema@icloud.com>
2024-12-30 13:36:01 +0100

    Wijziging

--- Sql/KillConnection.sql
+++ Sql/KillConnection.sql
@@ -5,10 +5,11 @@ declare @SqlCommand varchar(max) = ''
 declare @PrintOnly bit = 1
 
 
-select @SqlCommand += 'kill ' + cast([session_id] as varchar(6)) + ';' + char(13)
+select	@SqlCommand += 'kill ' + 
+			cast([sessions].[session_id] as varchar(6)) + ';' + char(13)
 from	sys.dm_exec_sessions	[sessions]
 where
-	[database_id] = db_id(@DatabaseName)
+	[sessions].[database_id] = db_id(@DatabaseName)
 
 
 if @PrintOnly = 1
